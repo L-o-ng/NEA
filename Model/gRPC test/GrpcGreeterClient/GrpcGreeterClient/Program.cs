c47ecc8a933ca8f092b14d46233e249a0b4cc339
Long <84541379+L-o-ng@users.noreply.github.com>
2023-10-26 17:02:00 +0100

    Add files via upload

---
+++ Model/gRPC test/GrpcGreeterClient/GrpcGreeterClient/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Grpc.Net.Client;
+using GrpcGreeterClient;
+
+
+using var channel = GrpcChannel.ForAddress("https://localhost:7169");
+var client = new Greeter.GreeterClient(channel);
+
+
+while (true)
+{
+	var reply = await client.SayHelloAsync(new HelloRequest { Name = Console.ReadKey().KeyChar.ToString() });
+	Console.WriteLine("Greeting: " + reply.Message);
+}
