caedd1cd7a3157905e6fd9ec65fb8bffcc9a6c51
Long <84541379+L-o-ng@users.noreply.github.com>
2023-11-08 10:58:12 +0000

    Added regen

--- MVP/Algorithm testing/Program.cs
+++ MVP/Algorithm testing/Program.cs
@@ -1,9 +1,18 @@
 ﻿using Algorithm_testing;
 
-Maze maze = new DepthFirstGeneration(20, 19);
-maze.InitMaze();
-maze.BuildMaze(maze.MazeCoordinates[1, 1]);
+while (true) {
+    Maze maze = new DepthFirstGeneration(20, 19);
+    maze.InitMaze();
+    maze.BuildMaze(maze.MazeCoordinates[1, 1]);
+    maze.MazeWalls[1, 0] = false;
+    maze.MazeWalls[maze.MazeActualHeight - 2, maze.MazeActualWidth - 1] = false;
+    PrintMaze(maze);
 
-PrintMaze(maze);
+    Console.ReadKey(true);
+    Console.Clear();
+}
+
+
+
 
 static void PrintMaze(Maze maze) { for (int i = 0; i < maze.MazeActualHeight; i++) { for (int j = 0; j < maze.MazeActualWidth; j++) { Console.Write(maze.MazeWalls[i, j] ? "\u2588\u2588" : "  "); } Console.WriteLine(); } }
