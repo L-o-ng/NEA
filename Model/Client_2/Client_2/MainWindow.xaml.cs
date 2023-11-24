d7fe28887c94db5280bf9d6133cc1f494a2a588a
Long <84541379+L-o-ng@users.noreply.github.com>
2023-11-24 14:49:52 +0000

    WPF Tested
    Looks promising, will have to maze it up for a few hours to see if it works.

---
+++ Model/Client_2/Client_2/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Client_2
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private unsafe void btn_hello_Click(object sender, RoutedEventArgs e)
+        {
+            txb_hello.Text = "Hello World";
+            ThreadPool.QueueUserWorkItem((state) =>
+            {
+                Thread.Sleep(1000);
+                Dispatcher.Invoke(() => txb_hello.Text = string.Empty);
+            });
+            
+        }
+    }
+}
