ca763d96895a8e558be561bb671fe2b00f4c6ea4
Long <dylant7192@gmail.com>
2024-01-11 21:32:30 +0000

    Updated tech docs

--- v1.0 Build/Server/Server/obj/Debug/net6.0/Server.AssemblyInfo.cs
+++ v1.0 Build/Server/Server/obj/Debug/net6.0/Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e85d87465d3cda10b52ca36a53cac82db040c9dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
